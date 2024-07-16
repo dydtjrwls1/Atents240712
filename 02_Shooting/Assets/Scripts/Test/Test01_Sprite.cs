6a7bfc5329092732a2c22b8092de22b45bb978ae
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-07-16 13:50:25 +0900

    2024-07-16
    editconfig 추가

---
+++ 02_Shooting/Assets/Scripts/Test/Test01_Sprite.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01_Sprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 첫번째 업데이트 함수가 실행되기 직전에 한번 호출되는 함수
+        Debug.Log("시작");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
