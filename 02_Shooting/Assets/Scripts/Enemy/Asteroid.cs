0cd43ca9c33ab71304edf0c6ac3425b194b52145
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-07-25 12:20:19 +0900

    2024-07-25 (4)
    회전 기초

---
+++ 02_Shooting/Assets/Scripts/Enemy/Asteroid.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Rotate(0, 0, Time.deltaTime * 360);                // x,y,x 따로 받기
+        // transform.Rotate(Time.deltaTime * 360 * Vector3.forward);    // vector3 로 받기
+        // transform.Rotate(Vector3.forward, Time.deltaTime * 360);     // 축과 축을 중심으로 얼마나 회전할지
+
+        
+    }
+}
