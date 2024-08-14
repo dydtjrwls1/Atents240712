78e235bdf2b6492f2740f9a636d284fa440e6d66
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-08-14 11:23:03 +0900

    2024-08-14-(2)

---
+++ 03_3D_Basic/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    public float rotateSpeed = 180.0f;
+
+    PlayerInputActions inputActions;
+}
