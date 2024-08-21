5e1395a60540fa07132d9c6bc62b1a194bba28e7
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-08-21 13:16:30 +0900

    switch finish

--- 03_3D_Basic/Assets/Scripts/DoorController/DoorSwitch.cs
+++ 03_3D_Basic/Assets/Scripts/DoorController/DoorSwitch.cs
@@ -9,10 +9,11 @@ public class DoorSwitch : MonoBehaviour, IInteractable
     // off 상태가 되었을 때 문이 닫힌다.
     // 연결되는 문은 수동문만 가능.
     // on/off 가 번갈아가면서 설정된다.
-
-    Animator animator;
+    public DoorManual door;
 
     public float coolDown = 1.0f;
+
+    Animator animator;
 
     float remainsCoolDown = 0.0f;
 
@@ -46,7 +47,8 @@ public class DoorSwitch : MonoBehaviour, IInteractable
                 isOn = true;
                 animator.SetBool(SwitchOn_Hash, isOn);
             }
-            
+
+            door.Use();
             remainsCoolDown = coolDown;
         }
     }
