f73d6e70c3e26799a0aaa9346725831ddcc7c510
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-08-26 11:50:34 +0900

    2024-08-26-(3)

--- 03_3D_Basic/Assets/Scripts/Trap/Trap_Fire.cs
+++ 03_3D_Basic/Assets/Scripts/Trap/Trap_Fire.cs
@@ -1,28 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class Trap_Fire : TrapBase
 {
     // 밟으면 fire 파티클 작동, 파티클의 재생시간이 끝나면 정지된다.
-    ParticleSystem fireParticle;
+    // 이펙트 재생 시간
+    public float duration = 5.0f;
+
+    // 조명 최대 밝기
+    public float maxLightIntencity = 2.7f;
+
+    // 조명 최대 범위
+    public float maxLightRange = 7.0f;
+
+    ParticleSystem ps;
+    Light effectLight;
 
     private void Awake()
     {
-        fireParticle = GetComponentInChildren<ParticleSystem>(true);
+        ps = GetComponentInChildren<ParticleSystem>();
+        effectLight = GetComponentInChildren<Light>();
+        // effectLight.intensity = 0.0f;                    // 일단 조명 꺼 놓기
+        effectLight.range = 0.0f;
     }
 
     protected override void OnTrapActivate(GameObject target)
     {
-        StartCoroutine(Activate());
+        ps.Play();                                              // 이펙트 재생하기
+        Player targetPlayer = target.GetComponent<Player>();
+        targetPlayer?.Die();
+
+        StopAllCoroutines();                                    // 이전에 재생하던 코루틴 정지
+        StartCoroutine(EffectCoroutine());                      // 이펙트 끄기용 코루틴 실행
     }
 
-    IEnumerator Activate()
+    IEnumerator EffectCoroutine()
     {
-        fireParticle.gameObject.SetActive(true);
-        fireParticle.Play();
-        yield return new WaitForSeconds(fireParticle.main.duration);
-        fireParticle.Pause();
-        fireParticle.gameObject.SetActive(false);
+        // yield return new WaitForSeconds(0.1f);                  // 이펙트 재생되고 0.1초 뒤에 조명 켜기
+
+        const float lightTime = 0.2f;
+        float remainsTime = lightTime;
+
+        while (remainsTime > 0)
+        {
+            // effectLight.intensity = Mathf.Lerp(0, maxLightIntencity, (remainsTime - lightTime) * -10);
+            effectLight.range = Mathf.Lerp(0, maxLightRange, (remainsTime - lightTime) * -10);
+            remainsTime -= Time.deltaTime;
+
+            yield return null;
+        }
+                      
+        yield return new WaitForSeconds(duration - 0.1f);       // 이펙트 재생되고 durtaion 초 뒤에 파티클 정지, 조명 끄기
+        ps.Stop();
+
+        remainsTime = lightTime;
+        while (remainsTime > 0)
+        {
+            // effectLight.intensity = Mathf.Lerp(0, maxLightIntencity, remainsTime * 10);
+            effectLight.range = Mathf.Lerp(0, maxLightRange, remainsTime * 10);
+            remainsTime -= Time.deltaTime;
+
+            yield return null;
+        }
+
+        effectLight.range = 0.0f;
     }
 }
--- 03_3D_Basic/Assets/Scripts/Trap/Trap_Slow.cs
+++ 03_3D_Basic/Assets/Scripts/Trap/Trap_Slow.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Trap_Slow : TrapBase
 {
-    // Start is called before the first frame update
-    void Start()
+    // 슬로우 지속 시간
+    public float slowDuration = 5.0f;
+
+    // 느려지는 비율
+    [Range(0.1f, 1f)]
+    public float slowRate = 0.5f;
+
+    ParticleSystem ps;
+    Light effectLight;
+
+    private void Awake()
     {
-        
+        Transform child = transform.GetChild(1);
+        ps = child.GetComponent<ParticleSystem>();
+        effectLight = GetComponentInChildren<Light>();
+        effectLight.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnTrapActivate(GameObject target)
     {
-        
+        effectLight.enabled = true;
+        ps.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        effectLight.enabled = false;
+        ps.Stop();
     }
 }
