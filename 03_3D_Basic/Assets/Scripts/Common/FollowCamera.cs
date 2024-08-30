778a40d123453bb48b68d2f5ef3e4457e7518fab
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-08-30 11:17:12 +0900

    camera raycast

--- 03_3D_Basic/Assets/Scripts/Common/FollowCamera.cs
+++ 03_3D_Basic/Assets/Scripts/Common/FollowCamera.cs
@@ -13,6 +13,9 @@ public class FollowCamera : MonoBehaviour
     // 대상과 카메라의 간격
     Vector3 offset;
 
+    // target 과 camera 사이의 실제 거리 (= offset 의 거리)
+    float length;
+
     void Start()
     {
         if(target == null)
@@ -22,18 +25,28 @@ public class FollowCamera : MonoBehaviour
         }
 
         offset = transform.position - target.position; // 타겟에서 카메라로 가는 방향 벡터
+        length = offset.magnitude;
     }
 
     private void FixedUpdate()
     {
-        // 대상 위치에서 회전된 offset 만큼 떨어진다 ( 회전 정도는 타겟의 y축 회전만큼 )
+        UpdateCamera();
+
+        Ray ray = new Ray(target.position, transform.position - target.position); // 카메라 root에서 카메라 방향으로 나가는 ray
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, length))
+        {
+            transform.position = hitInfo.point; // 충돌한 위치로 즉시 옮기기
+        }
+    }
+
+    // void LateUpdate() { } 물리와 일반 업데이트 차이 때문에 떨리는 현상이 발생.
+
+    void UpdateCamera()
+    {
         transform.position = Vector3.Slerp(
             transform.position,
             target.position + Quaternion.LookRotation(target.forward) * offset,
             Time.fixedDeltaTime * smooth);
-
         transform.LookAt(target);
     }
-
-    // void LateUpdate() { } 물리와 일반 업데이트 차이 때문에 떨리는 현상이 발생.
 }
