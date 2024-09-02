6056c89c20ab33dd43a38dc6eb385621a375c647
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-09-02 12:50:37 +0900

    maze start

--- 03_3D_Basic/Assets/Scripts/Maze/Maze.cs
+++ 03_3D_Basic/Assets/Scripts/Maze/Maze.cs
@@ -1,22 +1,27 @@
+using UnityEngine;
 
-
-public class Maze
+public class Maze : MonoBehaviour
 {
     public int length = 10;
 
-    CellBase[,] cells;
+    CellBase[,] grid;
 
     public Maze(int length)
     {
         this.length = length;
-        cells = new CellBase[length, length];
+        grid = new CellBase[length, length];
 
         for(int i = 0; i < length; i++)
         {
             for(int j = 0; j < length; j++)
             {
-                cells[i, j] = new CellBase(i, j);
+                grid[i, j] = new CellBase(i, j);
             }
         }
+    }
+
+    public void Init()
+    {
+
     }
 }
