dd9f68a84b6fdc76ad4c60f0364d0e7ab89fced6
dydtjrwls1 <59675426+dydtjrwls1@users.noreply.github.com>
2024-09-22 23:58:46 +0900

    astar

--- 04_Tilemap/Assets/Scripts/AStar/AStar.cs
+++ 04_Tilemap/Assets/Scripts/AStar/AStar.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public static class AStar
@@ -12,6 +14,8 @@ public static class AStar
     //const float diagonalDistance = 1.4142135f;
     const float diagonalDistance = 14f;
 
+    
+
     /// <summary>
     /// 경로를 찾는 함수
     /// </summary>
@@ -21,7 +25,9 @@ public static class AStar
     /// <returns>시작 위치에서 도착위치 까지의 경로 (길을 못찾으면 null)</returns>
     public static List<Vector2Int> PathFind(GridMap map, Vector2Int start, Vector2Int end)
     {
-        List<Vector2Int> path = null;
+        map.ClearMapData();
+
+        List<Vector2Int> path = new List<Vector2Int>();
 
         List<Node> openList = new List<Node>();
         List<Node> closeList = new List<Node>();
@@ -30,27 +36,69 @@ public static class AStar
         closeList.Add(current);
         path.Add(start);
 
+        bool isDiagonal = true;
 
-
-        // 주위 노드를 openlist 에 넣는다
-        Vector2Int currentGrid = new(current.X, current.Y);
-
-        for (int i = -1; i < 2; i++)
+        while(current != end)
         {
-            for (int j = -1; j < 2; j++)
-            {
-                if (i == 0 && j == 0) continue;
+            // 주위 노드를 openlist 에 넣는다 (Plain 이고 CloseList에 없을 경우)
+            Vector2Int currentGrid = new(current.X, current.Y);
 
-                Vector2Int nodeGrid = new(current.X + i, current.Y + j);
-                
-                if (map.IsPlain(nodeGrid))
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
                 {
+                    Vector2Int nodeGrid = new(current.X + i, current.Y + j);
+                    Node node = map.GetNode(nodeGrid);
+
+                    // Plain 이 아니거나 노드가 Grid 범위 밖일 경우거나 CloseList에 포함되어 있을경우 넘어간다.
+                    if (!map.IsPlain(nodeGrid) || node == null || closeList.Contains(node))
+                    {
+                        isDiagonal = !isDiagonal;
+                        continue;
+                    }
+
+
+                    float currentG = isDiagonal ? diagonalDistance : sideDistance;
+
+                    if(currentG < node.G)
+                    {
+                        node.G = currentG < node.G ? currentG : node.G;
+                        node.prev = current;
+                    }
                     
-                    openList.Add(map.GetNode(nodeGrid));
+                    node.H = GetHeuristic(node, end);
+
+                    if (!openList.Contains(node))
+                    {
+                        openList.Add(map.GetNode(nodeGrid));
+                    }
+
+                    isDiagonal = !isDiagonal;
                 }
             }
+
+
+            openList.Sort();
+
+            Node nextNode = openList.First();
+            nextNode.prev = current;
+            current = nextNode;
+            openList.RemoveAt(0);
+            closeList.Add(current);
         }
+        
+        foreach(Node node in closeList)
+        {
+            Debug.Log($"{node.X}, {node.Y}");
+            Debug.Log($"F = {node.F}");
+        }
+
+
+        
+
         // 주위 노드의 f(x) 값을 구한다
+
+
 
         return path;
     }
@@ -60,7 +108,6 @@ public static class AStar
     // A* 알고리즘의 휴리스틱 값 계산하는 함수 ( 현재 위치에서 목적지 까지의 예상거리 )
     private static float GetHeuristic(Node current, Vector2Int end)
     {
-
-        return 0;
+        return (Mathf.Abs(end.x - current.X) + Mathf.Abs(end.y - current.Y)) * sideDistance;
     }
 }
