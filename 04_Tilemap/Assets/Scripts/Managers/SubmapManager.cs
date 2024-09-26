1b03c59307ebe4d4171fccea5a2099dcc228fb03
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-09-26 11:44:15 +0900

    2024-09-26-(3)

--- 04_Tilemap/Assets/Scripts/Managers/SubmapManager.cs
+++ 04_Tilemap/Assets/Scripts/Managers/SubmapManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SubmapManager : MonoBehaviour
 {
@@ -62,13 +64,144 @@ public class SubmapManager : MonoBehaviour
         }
     }
 
-    public void PreInitialize()
+    private void Update()
     {
-        
+        // 완료된 작업은 작업 리스트에서 제외
+        foreach(int index in loadWorkComplete)
+        {
+            loadWork.RemoveAll((x) => x == index); // loadWork 에서 값이 index인 항목은 모두 제거
+        }
+        loadWorkComplete.Clear(); // 다 제거했으니 리스트 비우기
+
+        // 들어온 요청
+        foreach(int index in loadWork)
+        {
+            AsyncSceneLoad(index);
+        }
+
+        foreach (int index in UnloadWorkComplete)
+        {
+            UnloadWork.RemoveAll((x) => x == index); // unloadWork 에서 값이 index인 항목은 모두 제거
+        }
+        UnloadWorkComplete.Clear(); // 다 제거했으니 리스트 비우기
+
+        foreach (int index in UnloadWork)
+        {
+            AsyncSceneUnload(index);
+        }
     }
 
+    // 처음 생성 시 단 한번만 실행되는 함수
+    public void PreInitialize()
+    {
+        int mapCount = WidthCount * HeighCount;
+        sceneNames = new string[mapCount];
+        sceneLoadStates = new SceneLoadState[mapCount];
+
+        for(int y = 0; y < HeighCount; y++)
+        {
+            for(int x = 0; x < HeighCount; x++)
+            {
+                int index = GetIndex(x, y);
+                sceneNames[index] = $"{SceneNameBase}_{x}_{y}";
+                sceneLoadStates[index] = SceneLoadState.Unload;
+            }
+        }
+    }
+
+    // 신이 Single 모드로 로드될 때마다 호출되는 초기화 함수
     public void Initialize()
     {
+        for(int i = 0; i < sceneLoadStates.Length; i++)
+        {
+            sceneLoadStates[i] = SceneLoadState.Unload;
+        }
 
+        // 플레이어 관련 초기화
+    }
+
+    /// <summary>
+    /// 비동기 로딩을 요청하는 함수
+    /// </summary>
+    void RequestAsyncSceneLoad(int x, int y)
+    {
+        int index = GetIndex(x, y);
+        if (sceneLoadStates[index] == SceneLoadState.Unload) // 해당 서브맵이 Unload 상태일 때만 작업 리스트에 추가한다.
+        {
+            loadWork.Add(index);
+        }
+    }
+
+    /// <summary>
+    /// 로딩 해제를 요청하는 함수
+    /// </summary>
+    void RequestAsyncSceneUnload(int x, int y)
+    {
+        int index = GetIndex(x, y);
+        if (sceneLoadStates[index] == SceneLoadState.Loaded) // 해당 서브맵이 Unload 상태일 때만 작업 리스트에 추가한다.
+        {
+            UnloadWork.Add(index);
+        }
+    }
+
+    void AsyncSceneLoad(int index)
+    {
+        if (sceneLoadStates[index] == SceneLoadState.Unload)    // Unload 상태인 것들만
+        {
+            sceneLoadStates[index] = SceneLoadState.PendingLoad; // 진행 중이라고 표시
+
+            // 씬 로딩
+            AsyncOperation async = SceneManager.LoadSceneAsync(sceneNames[index], LoadSceneMode.Additive);
+            async.completed += (_) =>  // 비동기 작업이 완료되면 실행되는 델리게이트에 람다 함수 추가
+            {
+                sceneLoadStates[index] = SceneLoadState.Loaded; // 완료되었으니 Loaded로 상태 변경
+                loadWorkComplete.Add(index);                    // 완료 리스트에 추가
+            };
+        }
+    }
+
+    void AsyncSceneUnload(int index)
+    {
+        if (sceneLoadStates[index] == SceneLoadState.Loaded)    // Loaded 상태인 것들만
+        {
+            sceneLoadStates[index] = SceneLoadState.PendingUnload; // 해제 진행 중이라고 표시
+
+            // 해제할 씬에 있는 슬라임을 풀로 되돌리기 (씬 언로드로 인한 삭제 방지)
+            Scene scene = SceneManager.GetSceneByName(sceneNames[index]);
+            if(scene.isLoaded)
+            {
+                GameObject[] rootObjects = scene.GetRootGameObjects(); // 루트 오브젝트 모두 찾기(부모없는 최상위 오브젝트들)
+                if(rootObjects != null && rootObjects.Length > 0)
+                {
+                    // 서브맵은 루트 오브젝트가 Grid 하나만 있음
+                    Slime[] slimes = rootObjects[0].GetComponentsInChildren<Slime>();  // 루트 오브젝트의 자손으로 있는 모든 슬라임 찾기
+                    foreach(Slime slime in slimes)
+                    {
+                        slime.ReturnToPool();
+                    }
+                }
+            }
+
+            // 씬 로딩 해제
+            AsyncOperation async = SceneManager.UnloadSceneAsync(sceneNames[index]);
+            async.completed += (_) =>  // 비동기 작업이 완료되면 실행되는 델리게이트에 람다 함수 추가
+            {
+                sceneLoadStates[index] = SceneLoadState.Unload; // 해제 완료되었으니 Unload로 상태 변경
+                UnloadWorkComplete.Add(index);                    // 해제 완료 리스트에 추가
+            };
+        }
+    }
+
+    // grid 를 index로 변경하는 함수
+    int GetIndex(int x, int y)
+    {
+        return x + y * HeighCount;
+    }
+
+    // 월드 좌표가 어떤 맵에 속하는지 계산하는 함수
+    public Vector2Int WorldToGrid(Vector3 world)
+    {
+        Vector2 offset = (Vector2)world - worldOrigin;
+        return new Vector2Int((int)(offset.x / SubmapWidthSize), (int)(offset.y / SubmapHeightSize));
     }
 }
